ce8fe683a2b3702ed3d32857c8d3409ab4f715c5
Anastasia Lavrova <44167571+anastasialavrova@users.noreply.github.com>
2021-01-15 00:50:09 +0300

    add

--- integration/api.py
+++ integration/api.py
@@ -6,7 +6,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity,  get_raw_jwt)
 
 
-from env import app, api, login_manager
+from env import app, login_manager
 from models import News, Doctors, Record, User
 
 
