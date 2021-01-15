354548d36dbb4771321604ad156825780afee64e
Anastasia Lavrova <44167571+anastasialavrova@users.noreply.github.com>
2021-01-16 01:32:38 +0300

    add e2e

--- integration/e2e_tests.py
+++ integration/e2e_tests.py
@@ -30,50 +30,33 @@ with app.app_context():
     class TestIntegrations(unittest.TestCase):
         def setUp(self):
             self.app = app.test_client()
+            app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+            db.create_all()
 
-        # def test_get_news(self):
-        #     expected_result = {"message": "List of news"}
-        #     r = requests.get('http://127.0.0.1:5000/news')
-        #     assert r.status_code == 200
-        #     assert expected_result == r.json()
+        def test_e2e(self):
+            r = requests.get('http://127.0.0.1:5000/')
+            assert r.status_code == 200
 
-        def test_post_news(self):
-            expected_result = {"text": "text", "title": "title"}
-            r = requests.post('http://127.0.0.1:5000/news?news_title=title&news_text=text')
+            expected_result = {"message": "List of news"}
+            r = requests.get('http://127.0.0.1:5000/news')
             assert r.status_code == 200
             assert expected_result == r.json()
 
-        def test_get_doctors(self):
             expected_result = {'message': 'List of doctors'}
             r = requests.get('http://127.0.0.1:5000/doctors')
             assert r.status_code == 200
             assert expected_result == r.json()
 
-        def test_post_records(self):
-            expected_result = {"diagnosis": "hello", "login_patient": "1"}
-            r = requests.post('http://127.0.0.1:5000/records?rec_login=1&rec_diag=hello')
+            r = requests.get('http://127.0.0.1:5000/user_space')
             assert r.status_code == 200
-            assert expected_result == r.json()
 
-
-        '''@patch('api.AddDiagnosis.find_in_db', return_value=1)
-        def test_find_None_record(self, find_in_db):
-            diagnosis = AddDiagnosis()
-            rec = diagnosis.find_record('login2', find_in_db)
-            assert rec == True
-
-        @patch('api.Users.find_in_db', return_value=UserBuild().build())
-        def test_sign_in_user(self, find_in_db):
-                user = Users('login1', 'password1')
-                assert user.sign_up(find_in_db) == 'user_space'
-                '''
-
-        def test_get_news(self):
-            expected_result = "Ok"
-            r = requests.get('http://127.0.0.1:5000/records/id')
-
+            r = requests.get('http://127.0.0.1:5000/user_space')
             assert r.status_code == 200
-            assert expected_result == r.json()
+
+        def tearDown(self):
+            db.session.remove()
+            db.drop_all()
+
 
 
 if __name__ == '__main__':
